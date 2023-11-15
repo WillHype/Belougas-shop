fe02dec038e6c9e85061c049e506c9e1b651b1c7
WillHype <WillHype@users.noreply.github.com>
2023-11-16 00:21:36 +0100

    Reverse changes

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import './Item.scss';
-import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
+import React from 'react'
+import './Item.scss'
+import { Link } from 'react-router-dom'
 
 const Item = (props) => {
-  const scrollToTop = () => {
-    scroll.scrollToTop({ duration: 1000, smooth: 'easeInOutQuart' });
+  const handleItemClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'  // Ajoute la propriété 'smooth' pour un défilement en douceur
+    });
   };
 
   return (
     <div className="item">
-      <ScrollLink to={`product-${props.id}`} smooth={true} duration={1000}>
-        <img onClick={scrollToTop} src={props.image} alt="" />
-      </ScrollLink>
+      <Link to={`/product/${props.id}`}>
+        <img onClick={handleItemClick} src={props.image} alt="" />
+      </Link>
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">€{props.new_price}</div>
-        <div className="item-price-old">€{props.old_price}</div>
+        <div className="item-price-new">
+          €{props.new_price}
+        </div>
+        <div className="item-price-old">
+          €{props.old_price}
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default Item;
