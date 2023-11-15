269c56573a991f0b5015366cbebba82195af5713
WillHype <WillHype@users.noreply.github.com>
2023-11-16 00:13:18 +0100

    Tweak on scroll to top on products

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -3,20 +3,29 @@ import './Item.scss'
 import { Link } from 'react-router-dom'
 
 const Item = (props) => {
+  const handleItemClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'  // Ajoute la propriété 'smooth' pour un défilement en douceur
+    });
+  };
+
   return (
     <div className="item">
-        <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
-        <p>{props.name}</p>
-        <div className="item-prices">
-            <div className="item-price-new">
-                €{props.new_price}
-            </div>
-            <div className="item-price-old">
-                €{props.old_price}
-            </div>
+      <Link to={`/product/${props.id}`}>
+        <img onClick={handleItemClick} src={props.image} alt="" />
+      </Link>
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">
+          €{props.new_price}
         </div>
+        <div className="item-price-old">
+          €{props.old_price}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Item
+export default Item;
