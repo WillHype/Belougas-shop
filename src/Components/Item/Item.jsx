c92c44c25c670794536bb005750c32c26e809af3
WillHype <WillHype@users.noreply.github.com>
2023-11-16 00:20:56 +0100

    Adding react-scroll because the previous tweak doesn't work online for some reasons

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,31 +1,24 @@
-import React from 'react'
-import './Item.scss'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Item.scss';
+import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
 
 const Item = (props) => {
-  const handleItemClick = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'  // Ajoute la propriété 'smooth' pour un défilement en douceur
-    });
+  const scrollToTop = () => {
+    scroll.scrollToTop({ duration: 1000, smooth: 'easeInOutQuart' });
   };
 
   return (
     <div className="item">
-      <Link to={`/product/${props.id}`}>
-        <img onClick={handleItemClick} src={props.image} alt="" />
-      </Link>
+      <ScrollLink to={`product-${props.id}`} smooth={true} duration={1000}>
+        <img onClick={scrollToTop} src={props.image} alt="" />
+      </ScrollLink>
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">
-          €{props.new_price}
-        </div>
-        <div className="item-price-old">
-          €{props.old_price}
-        </div>
+        <div className="item-price-new">€{props.new_price}</div>
+        <div className="item-price-old">€{props.old_price}</div>
       </div>
     </div>
   );
-}
+};
 
 export default Item;
